6ff6bc6587b839d895d46129c23403698030a59b
bad64 <louvincent42@gmail.com>
2018-12-12 22:54:17 +0100

    Complete rewrite of the code for better RNG && modifiers support

--- main.c
+++ main.c
@@ -1,23 +1,28 @@
-#include "dice.h"
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
 
 int randint(int minvalue, int maxvalue)
 {
-	return rand() % (maxvalue+1 - minvalue) + minvalue;
+	//Source: https://stackoverflow.com/questions/2509679/how-to-generate-a-random-integer-number-from-within-a-range
+	
+	int r;
+    const unsigned int range = 1 + maxvalue - minvalue;
+    const unsigned int buckets = RAND_MAX / range;
+    const unsigned int limit = buckets * range;
+
+    do
+    {
+        r = rand();
+    } while (r >= limit);
+
+    return minvalue + (r / buckets);
 }
 
 int main(int argc, char* argv[])
 {
-    //Command line args
-    int i;
-    int ascii = 0;
-    for (i = 0; i < argc; i++)
-    {
-        if (strcmp(argv[i], "--ascii") == 0)
-        {
-            ascii = 1;
-        }
-    }
-
+	int i;
 	//Seed the RNG
 	char *username;
     #if (defined (_WIN32) || defined (_WIN64))
@@ -63,152 +68,320 @@ int main(int argc, char* argv[])
         printf("    <y>: Number of faces on each dice\n\n");
         printf("You can roll up to 20 dice per throw. (e.g.: 20d20, or 10d10 + 5d6 + 5d4)\n");
     }
-    else
+    else	//Enter main loop
     {
-		char *buffer_faces = (char *)calloc(10, sizeof(char));
-		char *buffer_dices = (char *)calloc(10, sizeof(char));
-		int individual_dice[21];
-		int faces, dices;
-		int j, k, l, m, result;
-
-        for (i = 1; i < argc; i++)
+		//Check if dice expression is ok
+		for (i = 1; i < argc; i++)
 		{
-			result = 0;
-			individual_dice[0] = '\0';
-			buffer_faces[0] = '\0';
-			buffer_dices[0] = '\0';
-			faces = 0;
-			dices = 0;
+			int dchar = 0, signchar = 0;
+			char *buf_dices, *buf_faces, *buf_mod;
+			
+			if (strchr(argv[i], 'd') != NULL)
+				dchar = (int)(strchr(argv[i], 'd') - argv[i]);
+			else	//Not a dice
+				continue;
+			
+			//Do we have a modifier to this roll ?
+			if (strchr(argv[i], '+') != NULL)
+				signchar = (int)(strchr(argv[i], '+') - argv[i]);
+			else if (strchr(argv[i], '-') != NULL)
+				signchar = (int)(strchr(argv[i], '-') - argv[i]);
+			else	//No modifier
+				signchar = 0;
+			
 
-			if (argv[i][0] == '+')
-            {
-                continue;
-            }
+			buf_dices = calloc(dchar + 1, sizeof(char));
+			memset(buf_dices, '\0', dchar);
 
-			for (j = 0; j < strlen(argv[i]); j++)
+			if (signchar == 0)	//No modifier
 			{
-				if (argv[i][j] != 'd')
-                {
-                    buffer_dices[j] = argv[i][j];
-                }
+				buf_faces = calloc(strlen(argv[i]) - dchar + 1, sizeof(char));
+				memset(buf_faces, '\0', strlen(argv[i]) - dchar + 1);
+			}
+			else
+			{
+				buf_faces = calloc(signchar - dchar + 1, sizeof(char));
+				memset(buf_faces, '\0', signchar - dchar + 1);
+			}
+			
+			if (dchar == 0)
+				buf_dices[0] = '1';
+			else
+				strncpy(buf_dices, argv[i], dchar);
+			
+			if (signchar == 0)	//No modifier
+			{
+				strncpy(buf_faces, &argv[i][dchar+1], strlen(argv[i]) - dchar - 1);
+				//printf("%s %s\n", buf_dices, buf_faces);
+			}
+			else
+			{
+				strncpy(buf_faces, &argv[i][dchar+1], signchar - dchar - 1);
+				
+				buf_mod = calloc(strlen(argv[i]) - signchar + 1, sizeof(char));
+				memset(buf_mod, '\0', strlen(argv[i]) - signchar + 1);
+				strncpy(buf_mod, &argv[i][signchar+1], strlen(argv[i]) - signchar + 1);
+				
+				//printf("%s %s %c%s\n", buf_dices, buf_faces, argv[i][signchar], buf_mod);
+			}
+			
+			int dices, faces, modifier;
+			
+			sscanf(buf_dices, "%d", &dices);
+			sscanf(buf_faces, "%d", &faces);
+			
+			if (signchar != 0)
+				sscanf(buf_mod, "%d", &modifier);
+			
+			if ((dices == 0) || (faces == 0))
+			{
+				continue;
+			}
+			else
+			{
+				//Proceed to actually roll
+				int result = 0;
+				
+				if (dices == 1)
+				{
+					printf("%dd%d: ", dices, faces);
+					
+					result = randint(1, faces);
+					
+					if (result == 1)
+					{
+						#if defined (WIN32)
+							if (modifier == 0)
+								printf("CRITICAL FAILURE !! You rolled a 1...\n");
+							else
+								printf("CRITICAL FAILURE !! You rolled a natural 1...\n");
+						#elif defined (__linux__)
+							if (modifier == 0)
+								printf("\e[1;91mCRITICAL FAILURE ! \e[21;91mYou rolled a 1...\e[0m");
+							else
+								printf("\e[1;91mCRITICAL FAILURE ! \e[21;91mYou rolled a natural 1...\e[0m");
+						#endif
+					}
+					else if (result == faces)
+					{
+						#if defined (WIN32)
+							if (modifier == 0)
+								printf("CRITICAL SUCCESS !! You rolled a %d !!\n", result);
+							else
+								printf("CRITICAL SUCCESS !! You rolled a natural %d !!\n", result);
+						#elif defined (__linux__)
+							if (modifier == 0)
+								printf("\e[1;92mCRTITICAL SUCCESS !! \e[21;92mYou rolled a %d !\e[0m", result);
+							else
+								printf("\e[1;92mCRTITICAL SUCCESS !! \e[21;92mYou rolled a natural %d !\e[0m", result);
+						#endif
+					}
+					else
+					{
+						if ((signchar != 0) && (modifier != 0))
+						{
+							if (argv[i][signchar] == '+')
+							{
+								printf("You rolled a %d ! (%d + %d)", result + modifier, result, modifier);
+							}
+							else if (argv[i][signchar] == '-')
+							{
+								if (result - modifier < 1)
+								{
+									printf("You rolled a 1 ! (%d + %d)", result, modifier);
+								}
+								else
+								{
+									printf("You rolled a %d ! (%d - %d)", result - modifier, result, modifier);
+								}
+							}
+						}
+						else
+						{
+							printf("You rolled a %d !", result);
+						}
+					}
+					printf("\n");
+				}
 				else
 				{
-					buffer_dices[j] = '\0';
-
-					for (k = j+1; k < strlen(argv[i]); k++)
+					printf("%dd%d: ", dices, faces);
+					int *rolls = calloc(faces, sizeof(int));
+					
+					int j;
+					for (j = 0; j < dices; j++)
 					{
-						buffer_faces[k-j-1] = argv[i][k];
+						rolls[j] = randint(1, faces);
 					}
-					buffer_faces[k-j-1] = '\0';
-
-					break;
+					
+					for (j = 0; j < dices; j++)
+						result += rolls[j];
+					
+					if (result == dices)
+					{
+						#if defined (WIN32)
+							if (modifier == 0)
+								printf("CRITICAL FAILURE !! You rolled a 1...\n");
+							else
+								printf("CRITICAL FAILURE !! You rolled a natural 1...\n");
+						#elif defined (__linux__)
+							if (modifier == 0)
+								printf("\e[1;91mCRITICAL FAILURE ! \e[21;91mYou rolled a 1...\e[0m");
+							else
+								printf("\e[1;91mCRITICAL FAILURE ! \e[21;91mYou rolled a natural 1...\e[0m");
+						#endif
+					}
+					else if (result == dices * faces)
+					{
+						#if defined (WIN32)
+							if (modifier == 0)
+								printf("CRITICAL SUCCESS !! You rolled a %d !!\n", result);
+							else
+								printf("CRITICAL SUCCESS !! You rolled a natural %d !!\n", result);
+						#elif defined (__linux__)
+							if (modifier == 0)
+								printf("\e[1;92mCRTITICAL SUCCESS !! \e[21;92mYou rolled a %d !\e[0m", result);
+							else
+								printf("\e[1;92mCRTITICAL SUCCESS !! \e[21;92mYou rolled a natural %d !\e[0m", result);
+						#endif
+					}
+					else
+					{
+						if ((signchar != 0) && (modifier != 0))
+						{
+							if (argv[i][signchar] == '+')
+							{
+								printf("You rolled a %d ! (", result + modifier);
+								
+								int k;
+								for (k = 0; k < dices; k++)
+								{
+									#if defined (WIN32)
+										if (k != dices-1)
+											printf("%d, ", rolls[k]);
+										else
+											printf("%d ", rolls[k]);
+									#elif defined (__linux__)
+										if (k != dices-1)
+										{
+											if (rolls[k] == 1)
+												printf("\e[1;91m%d\e[0m, ", rolls[k]);
+											else if (rolls[k] == faces)
+												printf("\e[1;92m%d\e[0m, ", rolls[k]);
+											else
+												printf("%d, ", rolls[k]);
+										}
+										else
+										{
+											printf("%d ", rolls[k]);
+										}
+									#endif
+								}
+								
+								printf("+ %d)", modifier);
+							}
+							else if (argv[i][signchar] == '-')
+							{
+								if (result - modifier < 1)
+								{
+									printf("You rolled a 1 ! (");
+								
+									int k;
+									for (k = 0; k < dices; k++)
+									{
+										#if defined (WIN32)
+											if (k != dices-1)
+												printf("%d, ", rolls[k]);
+											else
+												printf("%d ", rolls[k]);
+										#elif defined (__linux__)
+											if (k != dices-1)
+											{
+												if (rolls[k] == 1)
+													printf("\e[1;91m%d\e[0m, ", rolls[k]);
+												else if (rolls[k] == faces)
+													printf("\e[1;92m%d\e[0m, ", rolls[k]);
+												else
+													printf("%d, ", rolls[k]);
+											}
+											else
+											{
+												printf("%d ", rolls[k]);
+											}
+										#endif
+									}
+									
+									printf("- %d)", modifier);
+								}
+								else
+								{
+									printf("You rolled a %d ! (", result - modifier);
+								
+									int k;
+									for (k = 0; k < dices; k++)
+									{
+										#if defined (WIN32)
+											if (k != dices-1)
+												printf("%d, ", rolls[k]);
+											else
+												printf("%d ", rolls[k]);
+										#elif defined (__linux__)
+											if (k != dices-1)
+											{
+												if (rolls[k] == 1)
+													printf("\e[1;91m%d\e[0m, ", rolls[k]);
+												else if (rolls[k] == faces)
+													printf("\e[1;92m%d\e[0m, ", rolls[k]);
+												else
+													printf("%d, ", rolls[k]);
+											}
+											else
+											{
+												printf("%d ", rolls[k]);
+											}
+										#endif
+									}
+									
+									printf("- %d)", modifier);
+								}
+							}
+						}
+						else
+						{
+							printf("You rolled a %d ! (", result - modifier);
+								
+							int k;
+							for (k = 0; k < dices; k++)
+							{
+								#if defined (WIN32)
+									if (k != dices-1)
+										printf("%d, ", rolls[k]);
+									else
+										printf("%d ", rolls[k]);
+								#elif defined (__linux__)
+									if (k != dices-1)
+									{
+										if (rolls[k] == 1)
+											printf("\e[1;91m%d\e[0m, ", rolls[k]);
+										else if (rolls[k] == faces)
+											printf("\e[1;92m%d\e[0m, ", rolls[k]);
+										else
+											printf("%d, ", rolls[k]);
+									}
+									else
+									{
+										printf("%d)", rolls[k]);
+									}
+								#endif
+							}
+						}
+					}
+					
+					printf("\n");
 				}
 			}
-
-			sscanf(buffer_faces, "%d", &faces);
-			sscanf(buffer_dices, "%d", &dices);
-
-			if ((dices == 0) || (faces < 4))
-            {
-                continue;
-            }
-
-			for (l = 0; l < dices; l++)
-			{
-			    int buffer = randint(1, faces);
-				result += buffer;
-				individual_dice[l] = buffer;
-			}
-			individual_dice[l] = 0;
-
-			if ((faces == 6) && (dices == 1))
-            {
-                if (ascii)
-                    d6(result);
-
-                if (result == dices)
-                {
-                    #if (defined (LINUX) || defined (__linux__))
-                        printf("\e[1;91mCRITICAL FAILURE ! \e[21;91mYou rolled a %d...\e[0m", result);
-                    #else
-                        printf("CRITICAL FAILURE ! You rolled a %d...", result);
-                    #endif
-                }
-                else if (result == dices * faces)
-                {
-                    #if (defined (LINUX) || defined (__linux__))
-                        printf("\e[1;92mCRTITICAL SUCCESS !! \e[21;92mYou rolled a %d !\e[0m", result);
-                    #else
-                        printf("CRTITICAL SUCCESS !! You rolled a %d !", result);
-                    #endif
-                }
-                else
-                    printf("You rolled a %d !", result);
-            }
-			else if ((faces == 20) && (dices == 1))
-            {
-                if (ascii)
-                    d20(result);
-
-                if (result == dices)
-                {
-                    #if (defined (LINUX) || defined (__linux__))
-                        printf("\e[1;91mCRITICAL FAILURE ! \e[21;91mYou rolled a %d...\e[0m", result);
-                    #else
-                        printf("CRITICAL FAILURE ! You rolled a %d...", result);
-                    #endif
-                }
-                else if (result == dices * faces)
-                {
-                    #if (defined (LINUX) || defined (__linux__))
-                        printf("\e[1;92mCRTITICAL SUCCESS !! \e[21;92mYou rolled a %d !\e[0m", result);
-                    #else
-                        printf("CRTITICAL SUCCESS !! You rolled a %d !", result);
-                    #endif
-                }
-                else
-                    printf("You rolled a %d !", result);
-            }
-            else
-            {
-                if (dices >= 2)
-                    printf("%s: ", argv[i]);
-
-                if (result == dices)
-                {
-                    #if (defined (LINUX) || defined (__linux__))
-                        printf("\e[1;91mCRITICAL FAILURE ! \e[21;91mYou rolled a %d...\e[0m", result);
-                    #else
-                        printf("CRITICAL FAILURE ! You rolled a %d...", result);
-                    #endif
-                }
-                else if (result == dices * faces)
-                {
-                    #if (defined (LINUX) || defined (__linux__))
-                        printf("\e[1;92mCRTITICAL SUCCESS !! \e[21;92mYou rolled a %d !\e[0m", result);
-                    #else
-                        printf("CRTITICAL SUCCESS !! You rolled a %d !", result);
-                    #endif
-                }
-                else
-                    printf("You rolled a %d !", result);
-
-                if (dices >= 2)
-                {
-                    printf(" (");
-
-                    for (m = 0; m < dices; m++)
-                    {
-                        if (m != dices-1)
-                            printf("%d, ", individual_dice[m]);
-                        else
-                            printf("%d)", individual_dice[m]);
-                    }
-                }
-            }
-
-            printf("\n");
 		}
     }
+	
     return 0;
 }
